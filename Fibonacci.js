bc6fd9d75113ded87bf8aa1c6fd8e0eadd280964
Abdul Raziq Quraishi <99569578+raziq-quraishi@users.noreply.github.com>
2026-02-01 08:01:59 +0430

    Learing Algorthims and Data Structures 1: Fibonacci

---
+++ Fibonacci.js
@@ -0,0 +1,16 @@
+// ------------------ Fibonacci Algorithm
+
+function fibonacci(n){
+    const fib = [0, 1];
+    for(let i = 2; i < n; i++){
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib
+}
+
+console.log(fibonacci(5))
+console.log(fibonacci(6))
+console.log(fibonacci(7))
+console.log(fibonacci(8))
+console.log(fibonacci(9))
+console.log(fibonacci(10))
